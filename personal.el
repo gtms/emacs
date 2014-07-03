9e3995bb2cd655fbe2e92c4f621c8273153ffa02
Gil Tomas <gil.tms@gmail.com>
2014-07-03 13:55:49 +0200

    Add relative-line-numbers to personally required packages

--- personal.el
+++ personal.el
@@ -136,6 +136,6 @@
 (setenv "SSH_AUTH_SOCK" (concat (getenv "HOME") "/.ssh-auth-sock"))
 
 ;; personally added packages
-(prelude-require-packages '(visual-regexp))
+(prelude-require-packages '(visual-regexp relative-line-numbers))
 
 ;;; personal.el ends here
