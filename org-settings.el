6b72e704555689b2b6c84f0d0d88a792205d36b5
Gil Tomas <gil.tms@gmail.com>
2015-02-05 14:23:03 +0100

    Modifications to org-settings.el

--- org-settings.el
+++ org-settings.el
@@ -9,9 +9,25 @@
 ;; sets org default directory
 (setq org-directory "~/Dropbox/org/")
 
+;; sets variable org-agenda-files
+(setf org-agenda-files (concat org-directory "todo/agenda-files"))
+
 ;; from 9.1.1 Setting up capture
-(setq org-default-notes-file (concat org-directory "notes/notes.org"))
+(setq org-default-notes-file (concat org-directory "todo/todo.org"))
 (define-key global-map "\C-cc" 'org-capture)
+
+;; from 5.2.1 TODO keywords as workflow states
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "APPT(a)" "|" "DONE(d)" "CANCELED(c)")))
+
+;; (setq org-refile-use-outline-path 'file)
+(setq org-refile-targets '((nil :maxlevel . 3)
+                                        ; all top-level headlines in the
+                                        ; current buffer are used (first) as a
+                                        ; refile target
+                           (org-agenda-files :maxlevel . 3)))
+
+(setq org-reverse-note-order t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; org-settings.el ends here
