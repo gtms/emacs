6631b66644b0cce12cd84c38f3bebfa0741e0fab
Gil Tomas <gtomas@ed.ac.uk>
2015-04-07 16:41:50 +0100

    [org-settings.el] Extensive revision of the file

--- org-settings.el
+++ org-settings.el
@@ -6,28 +6,68 @@
 ;; To be symlinked into ~/.emacs.d/personal/
 
 ;;; Code:
-;; sets org default directory
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Most of these settings are inspired by the excellent 'Org Mode -
+;; Organize Your Life In Plain Text!' manual, available online here:
+;; http://doc.norang.ca/org-mode.html
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; directory
 (setq org-directory "~/Dropbox/org/")
 
 ;; sets variable org-agenda-files
-(setf org-agenda-files (concat org-directory "todo/agenda-files"))
+(setq org-agenda-files (concat org-directory "todo/agenda-files"))
 
 ;; from 9.1.1 Setting up capture
 (setq org-default-notes-file (concat org-directory "todo/todo.org"))
 (define-key global-map "\C-cc" 'org-capture)
 
+;; (setq org-todo-keywords
+;;       (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+;;               (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "APPT"))))
+
+;; (setq org-todo-keyword-faces
+;;       (quote (("TODO" :foreground "red" :weight bold)
+;;               ("NEXT" :foreground "blue" :weight bold)
+;;               ("DONE" :foreground "forest green" :weight bold)
+;;               ("WAITING" :foreground "orange" :weight bold)
+;;               ("HOLD" :foreground "magenta" :weight bold)
+;;               ("CANCELLED" :foreground "forest green" :weight bold)
+;;               ("APPT" :foreground "forest green" :weight bold)
+;;               ("PHONE" :foreground "forest green" :weight bold))))
+
 ;; from 5.2.1 TODO keywords as workflow states
 (setq org-todo-keywords
-      '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "APPT(a)" "|" "DONE(d)" "CANCELED(c)")))
+      '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "APPT(a)" "|" "DONE(d)" "CANCELLED(c)")))
 
 ;; (setq org-refile-use-outline-path 'file)
-(setq org-refile-targets '((nil :maxlevel . 3)
-                                        ; all top-level headlines in the
-                                        ; current buffer are used (first) as a
-                                        ; refile target
-                           (org-agenda-files :maxlevel . 3)))
+(setq org-refile-targets (quote ((nil :maxlevel . 3)
+                                 (org-agenda-files :maxlevel . 3))))
 
-(setq org-reverse-note-order t)
+;; Use full outline paths for refile targets; we file directly with IDO
+(setq org-refile-use-outline-path t)
+
+;; Targets complete directly with IDO
+(setq org-outline-path-complete-in-steps nil)
+
+;; Allow refile to create parent tasks with confirmation
+;; (setq org-refile-allow-creating-parent-nodes (quote confirm))
+
+;; Use IDO for both buffer and file completion and ido-everywhere to t
+(setq org-completion-use-ido t)
+(setq ido-everywhere t)
+(setq ido-max-directory-size 100000)
+(ido-mode (quote both))
+
+;; Use the current window when visiting files and buffers with ido
+;; (setq ido-default-file-method 'selected-window)
+;; (setq ido-default-buffer-method 'selected-window)
+
+;; Use the current window for indirect buffer display
+;; (setq org-indirect-buffer-display 'current-window)
+
+;; (setq org-reverse-note-order t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; org-settings.el ends here
