b5df0d76e78f832c376579aff3eb5ec75400ec91
Gil Tomas <gtomas@ed.ac.uk>
2015-07-21 15:46:12 +0100

    [preload.el] Write preload.el

---
+++ preload.el
@@ -0,0 +1,21 @@
+;;; preload.el --- personal modifications to take effect prior to Emacs Prelude
+;;; loading
+
+;;; Commentary:
+;; This program contains personal modifications to Emacs Prelude
+;; To be symlinked into ~/.emacs.d/personal/
+
+;;; Code:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; section: colour themes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; zenburn if on OS X; solarized-light if on gnu/linux
+
+(cond
+ ((string-equal system-type "gnu/linux")
+  (setq prelude-theme 'solarized-light)
+  )
+ )
+
+;;; preload.el ends here
